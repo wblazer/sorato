c7620fa8efd3410fc806de4e503b62d19dc914f5
William Blazer <william0blazer@gmail.com>
2026-05-12 22:00:31 -0500

    remove animations from dialogs

--- packages/web/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
+++ packages/web/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
@@ -24,7 +24,7 @@
 		data-slot="alert-dialog-content"
 		data-size={size}
 		class={cn(
-			"data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 bg-popover text-foreground ring-border gap-3 rounded-xl p-4 ring-1 duration-100 data-[size=default]:max-w-xs data-[size=sm]:max-w-64 data-[size=default]:sm:max-w-sm group/alert-dialog-content fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 outline-none",
+			"bg-popover text-foreground ring-border gap-3 rounded-xl p-4 ring-1 data-[size=default]:max-w-xs data-[size=sm]:max-w-64 data-[size=default]:sm:max-w-sm group/alert-dialog-content fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 outline-none",
 			className
 		)}
 		{...restProps}
--- packages/web/src/lib/components/ui/dialog/dialog-overlay.svelte
+++ packages/web/src/lib/components/ui/dialog/dialog-overlay.svelte
@@ -13,7 +13,7 @@
   bind:ref
   data-slot="dialog-overlay"
   class={cn(
-    'fixed inset-0 isolate z-50 bg-background/70 data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 duration-100 supports-backdrop-filter:backdrop-blur-xs',
+    'fixed inset-0 isolate z-50 bg-background/70',
     className
   )}
   {...restProps}
